4e90a2858c2a2346d2e6c498e09fec3871992bee
will258012 <will258012@gmail.com>
2024-01-28 20:05:42 +0800

    清理代码

--- Dialogs/UnhandledExceptionDialog.cs
+++ Dialogs/UnhandledExceptionDialog.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Will_ExtRecover.Dialogs
@@ -20,35 +12,18 @@ namespace Will_ExtRecover.Dialogs
         public UnhandledExceptionDialog(Exception ex)
         {
             InitializeComponent();
-            textBox1.Text = String.Format("异常类型：{0}\r\n异常消息：{1}\r\n 堆栈跟踪：\r\n {2} \r\n",
+            textBox1.Text = String.Format("异常类型：{0}\r\n异常消息：{1}\r\n 堆栈跟踪：\r\n {2} \r\n {3}",
                     ex.GetType().FullName, ex.Message, ex.StackTrace);//生成错误报告
             About aboutInstance = new About();
             label2.Text = aboutInstance.AssemblyTitle + " " + aboutInstance.AssemblyVersion;//获取版本号
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Program.OpenFile("https://github.com/will258012/Will_ExtRecover/issues/new");
         }
-
-        private void UnhandledExceptionDialog_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void UnhandledExceptionDialog_Load(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
     }
 }
