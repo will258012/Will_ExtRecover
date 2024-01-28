9b301c458804ad07b9e63087adb79923c33af3dd
will258012 <will258012@gmail.com>
2024-01-28 20:00:30 +0800

    更新“选择文件或文件夹”窗口
    - 完善注释
    - 清理代码
    - 添加ToolTip提示

--- Dialogs/ChooseFilesOrFoldersDialog.Designer.cs
+++ Dialogs/ChooseFilesOrFoldersDialog.Designer.cs
@@ -28,27 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.OpenFilebutton = new System.Windows.Forms.Button();
             this.FolderBrowserbutton = new System.Windows.Forms.Button();
             this.Aboutbutton = new System.Windows.Forms.Button();
             this.Acceptbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.textBox1.Location = new System.Drawing.Point(194, 30);
+            this.textBox1.Location = new System.Drawing.Point(183, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(493, 27);
             this.textBox1.TabIndex = 0;
+            this.textBox1.ModifiedChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Location = new System.Drawing.Point(15, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(162, 20);
             this.label1.TabIndex = 1;
@@ -58,7 +61,7 @@
             // OpenFilebutton
             // 
             this.OpenFilebutton.Cursor = System.Windows.Forms.Cursors.Default;
-            this.OpenFilebutton.Location = new System.Drawing.Point(477, 63);
+            this.OpenFilebutton.Location = new System.Drawing.Point(463, 63);
             this.OpenFilebutton.Name = "OpenFilebutton";
             this.OpenFilebutton.Size = new System.Drawing.Size(101, 33);
             this.OpenFilebutton.TabIndex = 2;
@@ -68,7 +71,7 @@
             // 
             // FolderBrowserbutton
             // 
-            this.FolderBrowserbutton.Location = new System.Drawing.Point(584, 63);
+            this.FolderBrowserbutton.Location = new System.Drawing.Point(573, 63);
             this.FolderBrowserbutton.Name = "FolderBrowserbutton";
             this.FolderBrowserbutton.Size = new System.Drawing.Size(103, 33);
             this.FolderBrowserbutton.TabIndex = 3;
@@ -78,23 +81,37 @@
             // 
             // Aboutbutton
             // 
-            this.Aboutbutton.Location = new System.Drawing.Point(12, 268);
+            this.Aboutbutton.Location = new System.Drawing.Point(11, 152);
             this.Aboutbutton.Name = "Aboutbutton";
             this.Aboutbutton.Size = new System.Drawing.Size(128, 31);
             this.Aboutbutton.TabIndex = 4;
-            this.Aboutbutton.Text = "关于...(A)";
+            this.Aboutbutton.Text = "关于...(&A)";
             this.Aboutbutton.UseVisualStyleBackColor = true;
             this.Aboutbutton.Click += new System.EventHandler(this.Aboutbutton_Click);
             // 
             // Acceptbutton
             // 
-            this.Acceptbutton.Location = new System.Drawing.Point(560, 268);
+            this.Acceptbutton.Location = new System.Drawing.Point(559, 152);
             this.Acceptbutton.Name = "Acceptbutton";
             this.Acceptbutton.Size = new System.Drawing.Size(128, 31);
             this.Acceptbutton.TabIndex = 5;
-            this.Acceptbutton.Text = "确定(O)";
+            this.Acceptbutton.Text = "确定(&O)";
             this.Acceptbutton.UseVisualStyleBackColor = true;
             this.Acceptbutton.Click += new System.EventHandler(this.Acceptbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(324, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "你可以把需要处理的文件或文件夹拖到此窗口，\r\n也可以点击右方按钮进行选取。\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
             // ChooseFilesOrFoldersDialog
             // 
@@ -102,7 +119,8 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 311);
+            this.ClientSize = new System.Drawing.Size(699, 197);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.Acceptbutton);
             this.Controls.Add(this.Aboutbutton);
             this.Controls.Add(this.FolderBrowserbutton);
@@ -117,6 +135,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Will_ExtRecover";
             this.Load += new System.EventHandler(this.ChooseFilesOrFoldersDialog_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.ChooseFilesOrFoldersDialog_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.ChooseFilesOrFoldersDialog_DragEnter);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,5 +150,7 @@
         private System.Windows.Forms.Button Acceptbutton;
         private System.Windows.Forms.Button Aboutbutton;
         private System.Windows.Forms.Button FolderBrowserbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- Dialogs/ChooseFilesOrFoldersDialog.cs
+++ Dialogs/ChooseFilesOrFoldersDialog.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -15,45 +8,89 @@ namespace Will_ExtRecover.Dialogs
 {
     public partial class ChooseFilesOrFoldersDialog : Form
     {
-        string[] args;
-        bool IsAOpenDialog = false;
+        /// <summary>
+        /// 需要处理的文件或目录的名录。
+        /// </summary>
+        private string[]? args;
+        /// <summary>
+        /// 用户是否正在文本框中键入。
+        /// </summary>
+        private bool IsTypeing = false;
 
-
+        /// <summary>
+        /// “选择文件或文件夹”窗口。
+        /// </summary>
         public ChooseFilesOrFoldersDialog()
         {
             InitializeComponent();
+            textBox1.GotFocus += new EventHandler(textBox1_FocusChanged);
+            textBox1.LostFocus += new EventHandler(textBox1_FocusChanged);
         }
-
+        /// <summary>
+        /// 当文本框文字改变时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if(!IsAOpenDialog)
+            if (IsTypeing) //如果是手动输入的路径
             {
-                // 使用正则表达式来匹配引号内的内容
-                var matches = Regex.Matches(textBox1.Text, "\"([^\"]*)\"");
-                args = matches.Cast<Match>().Select(m => m.Groups[1].Value).ToArray();
+                // 检查格式是否正确
+                if (!IsInputFormatCorrect(textBox1.Text))
+                {
+                    // 显示提示
+                    toolTip1.Show("输入格式错误，请确保路径用引号包围。", textBox1,3000);
+                }
+                else
+                {
+                    toolTip1.Hide(textBox1);
+                    // 使用正则表达式来匹配引号内的内容
+                    var matches = Regex.Matches(textBox1.Text, "\"([^\"]*)\"");
+                    args = matches.Cast<Match>().Select(m => m.Groups[1].Value).ToArray();//存储变量
+                    
+                }
             }
         }
-
-
+        /// <summary>
+        /// 检查是否每个路径都被引号包围
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>true 表示格式正确，返回 false 表示格式错误</returns>
+        private bool IsInputFormatCorrect(string input)
+        {
+            return Regex.IsMatch(input, "^\"([^\"]*)\"( \"([^\"]*)\")*$");
+        }
+        private void textBox1_FocusChanged(object sender, EventArgs e) 
+        {
+            IsTypeing = textBox1.Focused; //设定为textBox1的聚焦情况
+        }
+        /// <summary>
+        /// 点击“打开文件”时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OpenFilebutton_Click(object sender, EventArgs e)
         {
-            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.Multiselect = true;
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();//选择文件对话框
+            openFileDialog.Multiselect = true; //多选
             openFileDialog.ShowDialog();
-            if(openFileDialog.FileNames.Length > 0)
+            if (openFileDialog.FileNames.Length > 0) //如果选择了文件
             {
-                args = openFileDialog.FileNames;
-                IsAOpenDialog = true;
+                args = openFileDialog.FileNames; //赋值给args
                 textBox1.Text = null;
                 foreach (var path in args)
                 {
-                    textBox1.Text = '"' + path + '"';
+                    textBox1.Text += '"' + path + '"' + " ";// 添加路径到文本框
                 }
-                IsAOpenDialog = false;
             }
-            
+
         }
-        private void FolderBrowserbutton_Click(object sender,EventArgs e)
+        /// <summary>
+        /// 点击“打开文件夹”按钮时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FolderBrowserbutton_Click(object sender, EventArgs e)
         {
 #if NET48           
 
@@ -61,43 +98,43 @@ namespace Will_ExtRecover.Dialogs
 #endif
 #if NET8_0_OR_GREATER
             var folderBrowser = new OpenFolderDialog(); //OpenFolderDialog 方法在.NET 8.0之后可用 
-            folderBrowser.Multiselect = true;
+            folderBrowser.Multiselect = true; //多选
 #endif
             folderBrowser.ShowDialog();
 #if NET48
-            if(folderBrowser.SelectedPath != string.Empty) { 
-            args = new string[1];
-            args[0] = folderBrowser.SelectedPath;
-            IsAOpenDialog = true;
-            textBox1.Text = '"' + folderBrowser.SelectedPath + '"';
-            IsAOpenDialog = false;
+            if (folderBrowser.SelectedPath != string.Empty)
+            {
+                args = new string[1];
+                args[0] = folderBrowser.SelectedPath; //FolderBrowserDialog 只能选取一个文件夹
+                textBox1.Text = '"' + folderBrowser.SelectedPath + '"'; // 添加路径到文本框
             }
 #endif
 #if NET8_0_OR_GREATER
-            if (folderBrowser.FolderNames.Length > 0){
-            args = folderBrowser.FolderNames;
-            textBox1.Text = null;
-            IsAOpenDialog = true;
-            foreach (var path in args)
+            if (folderBrowser.FolderNames.Length > 0)
             {
-                textBox1.Text += '"' + path + '"';
+                args = folderBrowser.FolderNames;
+                textBox1.Text = null;
+                foreach (var path in args)
+                {
+                    textBox1.Text += '"' + path + '"' + " "; // 添加路径到文本框
+                }
             }
-            IsAOpenDialog = false;
-        }
 #endif
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// 点击“确定”按钮时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Acceptbutton_Click(object sender, EventArgs e)
         {
             // 检查 args 是否已正确填充
             if (args == null)
             {
-                MessageBox.Show("没有选择文件或文件夹！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //MessageBox.Show("没有选择文件或文件夹！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error, 
+                //  MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                // 显示提示
+                toolTip1.Show("未输入/选择文件或文件夹！", textBox1, 3000);
                 return;
             }
             Entry.args = args;
@@ -108,24 +145,55 @@ namespace Will_ExtRecover.Dialogs
             if (args.Length > 1)
             {
                 MessageBox.Show("处理完毕！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information,
-                MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
             }
-          //  this.Close();    
-        }
-        private void ChooseFilesOrFoldersDialog_Load(object sender, EventArgs e)
-        {
-
         }
 
+        /// <summary>
+        /// 点击“关于”按钮时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Aboutbutton_Click(object sender, EventArgs e)
         {
             using var about = new About();
             about.ShowDialog();
         }
 
-        private void progressBar1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 拖入时执行的内容。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChooseFilesOrFoldersDialog_DragDrop(object sender, DragEventArgs e)
         {
+            // 检查是否有东西拖入
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // 获取拖入的文件或文件夹路径
+                string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
 
+                // 将路径写入 args 数组
+                args = paths;
+                // 清空文本框并添加新路径
+                textBox1.Text = null;
+                foreach (var path in paths)
+                {
+                    textBox1.Text += '"' + path + '"' + " "; // 添加路径到文本框
+                }
+            }
         }
+        private void ChooseFilesOrFoldersDialog_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        private void ChooseFilesOrFoldersDialog_Load(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e) { }
     }
 }
